17421fe11cd6d9586b21acbd277e77fb6d373d71
AbelW <unoctium1@gmail.com>
2019-02-17 15:20:54 -0800

    Work on goal

---
+++ Unity/Assets/Scripts/UDPManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UDPManager : MonoBehaviour {
+
+    private float lastSentDist;
+    [SerializeField]
+    private Goal goal;
+    private const float THRESHOLD = 0.05f;
+
+    // Use this for initialization
+    void Start () {
+        lastSentDist = goal.GetDistance();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Math.Abs(goal.GetDistance() - lastSentDist) > THRESHOLD)
+        {
+
+        }
+	}
+}
