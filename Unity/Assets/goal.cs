58eaf001f7cf1c908fc1ed54a8a323b820744aca
AbelW <unoctium1@gmail.com>
2019-02-17 13:47:02 -0800

    Add goal, basic movement

---
+++ Unity/Assets/goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject goalObj;
+    [SerializeField]
+    private GameObject player;
+
+    private Collider goalCol;
+    private float distanceFromGoal;
+
+	// Use this for initialization
+	void Start () {
+        goalCol = goalObj.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distanceFromGoal = (goalObj.transform.position - player.transform.position).magnitude;
+	}
+
+    public float GetDistance()
+    {
+        return distanceFromGoal;
+    }
+}
